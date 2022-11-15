77d3ab968a28b253024ab4e4a3c5c88c59382703
Balazs Horvath <balazs_horvath@epam.com>
2022-11-15 13:48:31 +0100

    style header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,20 @@
+import logo from './logo.svg';
+import style from './Header.module.css';
+
 const Header = () => {
   return (
-    <div className="Header">
-      <h1>Header</h1>
-    </div>
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid d-flex justify-content-around">
+        <a className="navbar-brand d-flex align-items-center" href="#">
+          <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block" />
+          TODO App
+        </a>
+        <div className="text-white d-flex">
+          <div>Tasks</div>
+          <div className={style['unchecked-tasks']}>35</div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
