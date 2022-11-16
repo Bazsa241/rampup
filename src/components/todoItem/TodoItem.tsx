08d84f5f3812d0ada937328762eef806d192194e
Balazs Horvath <balazs_horvath@epam.com>
2022-11-16 12:44:26 +0100

    add todo type

--- src/components/todoItem/TodoItem.tsx
+++ src/components/todoItem/TodoItem.tsx
@@ -1,10 +1,12 @@
-const TodoItem = () => {
+import { ITodo } from '../../types/todo';
+
+const TodoItem = ({ id, description, isDone }: ITodo) => {
   return (
     <div className="d-flex align-items-center justify-content-between border-bottom pb-2">
       <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-        <label className="form-check-label" htmlFor="flexCheckDefault">
-          Default checkbox
+        <input className="form-check-input" type="checkbox" checked={isDone} id={id} />
+        <label className="form-check-label" htmlFor={id}>
+          {description}
         </label>
       </div>
       <div className="d-flex gap-1">
--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -1,11 +1,18 @@
 import TodoItem from '../todoItem/TodoItem';
+import { ITodo } from '../../types/todo';
+
+const todos: ITodo[] = [
+  { id: '1', description: 'First todo', isDone: false },
+  { id: '2', description: 'Second todo', isDone: true },
+  { id: '3', description: 'Third todo', isDone: false }
+];
 
 const TodoList = () => {
   return (
     <div className="container d-grid gap-2">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todos.map(todo => (
+        <TodoItem {...todo} key={todo.id} />
+      ))}
     </div>
   );
 };
