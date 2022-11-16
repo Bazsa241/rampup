691ecd75d2ee9a4bbcbd21ee615565a1cfae6f2c
Balazs Horvath <balazs_horvath@epam.com>
2022-11-16 11:38:59 +0100

    set max-width

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <NewItem />
-      <TodoList />
+      <div className="wrapper">
+        <NewItem />
+        <TodoList />
+      </div>
       {/* <Counter /> */}
     </div>
   );
--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -3,11 +3,9 @@ import TodoItem from '../todoItem/TodoItem';
 const TodoList = () => {
   return (
     <div className="container d-grid gap-2">
-      {/* <div className="d-grid gap-2"> */}
       <TodoItem />
       <TodoItem />
       <TodoItem />
-      {/* </div> */}
     </div>
   );
 };
