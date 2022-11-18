d23548868cb1fd3d9b4992533e3deb365ed0fc5a
Balazs Horvath <balazs_horvath@epam.com>
2022-11-18 12:36:19 +0100

    delete & mark done functionality

--- src/components/todoItem/TodoItem.tsx
+++ src/components/todoItem/TodoItem.tsx
@@ -1,10 +1,20 @@
 import { ITodo } from '../../types/todo';
+import { useAppDispatch } from '../../app/hooks';
+import { removeTodo, editTodo } from '../../app/reducers/todoSlice';
 
 const TodoItem = ({ id, description, isDone }: ITodo) => {
+  const dispatch = useAppDispatch();
+
   return (
     <div className="d-flex align-items-center justify-content-between border-bottom pb-2">
       <div className="form-check">
-        <input className="form-check-input" type="checkbox" checked={isDone} id={id} />
+        <input
+          className="form-check-input"
+          type="checkbox"
+          checked={isDone}
+          onChange={() => dispatch(editTodo({ id, isDone: !isDone }))}
+          id={id}
+        />
         <label className="form-check-label" htmlFor={id}>
           {description}
         </label>
@@ -13,7 +23,7 @@ const TodoItem = ({ id, description, isDone }: ITodo) => {
         <button className="btn btn-sm btn-outline-secondary">
           <i className="bi bi-pencil-fill"></i>
         </button>
-        <button className="btn btn-sm btn-outline-danger">
+        <button className="btn btn-sm btn-outline-danger" onClick={() => dispatch(removeTodo(id))}>
           <i className="bi bi-trash-fill"></i>
         </button>
       </div>
--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -1,13 +1,10 @@
 import TodoItem from '../todoItem/TodoItem';
-import { ITodo } from '../../types/todo';
-
-const todos: ITodo[] = [
-  { id: '1', description: 'First todo', isDone: false },
-  { id: '2', description: 'Second todo', isDone: true },
-  { id: '3', description: 'Third todo', isDone: false }
-];
+import { useAppSelector } from '../../app/hooks';
+import { selectTodos } from '../../app/reducers/todoSlice';
 
 const TodoList = () => {
+  const todos = useAppSelector(selectTodos);
+
   return (
     <div className="container d-grid gap-2">
       {todos.map(todo => (
