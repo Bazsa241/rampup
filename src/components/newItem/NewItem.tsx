7dbaae6c1f2a7327ad46c93c987cd2f8e97e4b7d
Balazs Horvath <balazs_horvath@epam.com>
2022-11-18 12:34:40 +0100

    handle new todo

--- src/components/newItem/NewItem.tsx
+++ src/components/newItem/NewItem.tsx
@@ -1,12 +1,38 @@
+import { useState, FormEvent } from 'react';
+import { useAppDispatch } from '../../app/hooks';
+import { addTodo } from '../../app/reducers/todoSlice';
+import { ITodo } from '../../types/todo';
+
 const NewItem = () => {
+  const dispatch = useAppDispatch();
+  const [inputState, setInputState] = useState('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newTodo: ITodo = {
+      id: Date.now().toString(),
+      description: inputState,
+      isDone: false
+    };
+    dispatch(addTodo(newTodo));
+    setInputState('');
+  };
+
   return (
     <div className="container my-5">
-      <div className="input-group">
-        <input type="password" className="form-control" id="todoDescription" placeholder="TODO..." />
-        <button type="submit" className="btn btn-primary">
+      <form className="input-group" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          id="todoDescription"
+          placeholder="TODO..."
+          value={inputState}
+          onChange={e => setInputState(e.target.value)}
+        />
+        <button className="btn btn-primary" type="submit">
           + Add
         </button>
-      </div>
+      </form>
     </div>
   );
 };
